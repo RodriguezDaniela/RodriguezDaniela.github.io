3db5989a74f82e71e8858b61ea592a1ecc1654dc
RodriguezDaniela <124410790+RodriguezDaniela@users.noreply.github.com>
2023-06-09 19:31:02 -0700

    Practica 6

--- Parcial3/practica 6.php
+++ Parcial3/practica 6.php
@@ -7,30 +7,82 @@
     <title>Juego del ahorcado</title>
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
-<body>
+<body style="background-color: rgb(171, 214, 255);">
     <?php
-    $nombre = $_POST ["jugador"];
-    $palabra = $_POST ["palabra"];
-    $vidas = $_POST ["vidas"];
-    ?>
-   <div class="container">
-    <div class="row"> 
-        <h1>Juego del Ahorcado</h1><hr>
-        <h3>Nombre del jugador: <?php echo $nombre; ?></h3>
-        <h2>Numero de vidas: <?php echo $vidas; ?></h2>
-        <?php 
-        $letras=str_split($palabra); //[0]='H', [1]='O', [2]='L', [3]='A'; 
-        for($i=0; $i<(count($letras)); $i++){
-            echo "<input type='text' name='frase[]' style='width:5%'>";
-        }
-        ?>
-       </div>
-        <div class="row mt-4">
-        <input type="text" name="letras" style="width:10%;" maxlength="1">
-        <input type="submit" value="Enviar letra" class="btn btn-primary" style="width:10%; margin-left:20px">
-       </div>
-     </div>
-   </div>
-</body>
-</html>
+        $nombre = $_POST["jugador"]; $palabra = $_POST["palabra"]; $vidas = $_POST["vidas"];
+        $b= false;
+        
 
+        if(isset($_POST["frase"])){($_POST["frase"]);
+                $frase = $_POST["frase"];
+
+            $letra=$_POST["letra"];
+            //Asigna la letra al array de frase
+            for($i=0 ; $i<count(str_split($palabra));$i++){
+                if($palabra[$i] == $letra){
+                    $frase[$i] = $letra;
+                    $b = true;
+                }
+            }
+
+            //Resta visa en caso de que no coincida la letra
+            if(!$b){
+                echo "<h4>La letra ".$letra." no existe</h4>";
+                $vidas--;
+            }
+            //Valida si de terminaron las vidas
+            if($vidas <= 0){
+                echo "<h1>Juego terminado se agotaron las vidas!</h1>";
+                echo "<a href='practica 6.html'>Regresar al inicio</a>";
+                return;
+            }
+            $terminado = false;
+            //Valida si ganó
+            for($j=0 ; $j<count(str_split($palabra)); $j++){
+                if(!$frase[$j]){
+                    $terminado = true;
+                }
+            }
+
+            if($terminado == false){
+                echo "<h1>Felicidades, ganaste!</h1>";
+                echo "<a href='practica 6.html'>Regresar al inicio</a>";
+                return;
+            }
+
+        }
+    ?>
+    <div class="container">
+        <form action="practica 6.php" method="POST">
+            <input type="hidden" value="<?php echo $nombre ?>" name="jugador">
+            <input type="hidden" value="<?php echo $palabra ?>" name="palabra">
+            <input type="hidden" value="<?php echo $vidas ?>" name="vidas">
+
+            <div class="row">
+                <h1>Juego del Ahorcado</h1><hr>
+                <h3>Nombre del jugador: <?php echo $nombre; ?></h3>
+                <h4>Número de vidas: <?php echo $vidas; ?></h4>
+                <?php
+                    $letras = str_split($palabra); //[0]='H', [1]='O', [2]='L', [3]='A';
+                    
+                    for($i=0; $i<(count($letras)); $i++){
+                        if(isset($_POST["frase"])){
+                        echo "<input type='text' name='frase[]' style='width: 5%;' value='".$frase[$i]."'>";
+                    }else{
+                        echo "<input type='text' name='frase[]' style='width: 5%;' value=''>";
+                    }
+                }
+                ?>
+            </div>
+            <div class="row mt-4">
+                <div>
+                    <input type="text" name="letra" maxlength="1" required>
+                </div>
+                <div>
+                    <input type="submit" value="Enviar letra" class="btn btn-primary"  style="background-color: rgb(84, 162, 214);">
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html> 
